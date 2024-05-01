6b2a3203aaa842da5e8fc5963548995d354adc02
easywoo8080 <easywoo8080@gmail.com>
2024-05-02 00:33:23 +0900

    실행중엔 add . 안되는 코틀린 스프링부트?

--- src/main/kotlin/com/example/demo/index/mapper/IndexMapper.kt
+++ src/main/kotlin/com/example/demo/index/mapper/IndexMapper.kt
@@ -3,8 +3,8 @@ package com.example.demo.index.mapper
 import org.apache.ibatis.annotations.Mapper
 import org.springframework.stereotype.Repository
 
-@Repository
-@Mapper
+//@Repository
+//@Mapper
 interface IndexMapper {
     fun getData(): List<String>
 }
