99665fc5c06a3f479826ae4e17d93d691caa6294
easywoo8080 <easywoo8080@gmail.com>
2025-03-18 20:10:31 +0900

    코틀린 정리

--- src/main/kotlin/com/example/demo/index/biz/IndexBiz.kt
+++ src/main/kotlin/com/example/demo/index/biz/IndexBiz.kt
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
-
+@Transactional
 @Service
 class IndexBiz @Autowired constructor(private val dao: IndexMapper) : Biz<Int, IndexVO> {
 
